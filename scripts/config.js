eebf77a83de0640adfa97ecc7e82f798eb26492e
Sazzad Bashar Rakib <sazzad__bashar@outlook.com>
2023-06-24 23:31:53 +0600

    updating commit

--- scripts/config.js
+++ scripts/config.js
@@ -1,16 +1,25 @@
-function openPlayerConfig() {
-    playerConfigOverlayElement.style.display = 'block';
-    backdropElement.style.display = 'block';
-  }
-  
-  function closePlayerConfig() {
-    playerConfigOverlayElement.style.display = 'none';
-    backdropElement.style.display = 'none';
-  }
+function openPlayerConfig(event) {
+  editedPlayer = +event.target.dataset.playerid;
+  playerConfigOverlayElement.style.display = "block";
+  backdropElement.style.display = "block";
+}
 
-  function savePlayerConfig(event) {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const enteredPlayername = formData.get('playername').trim();
+function closePlayerConfig() {
+  playerConfigOverlayElement.style.display = "none";
+  backdropElement.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
+  errorsOutputElement.textContent = "";
+}
 
+function savePlayerConfig(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayername = formData.get("playername").trim(); // '      ' => ''
+
+  if (!enteredPlayername) {
+    // enteredPlayername === ''
+    event.target.firstElementChild.classList.add("error");
+    errorsOutputElement.textContent = "Please enter a valid name!";
+    return;
   }
+}
